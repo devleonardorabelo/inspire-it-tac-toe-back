bd51f1a0d567d2865f18d5a7c4516b2dda6c0982
Leonardo Rabelo <lrabelo.mc4@digio.com.br>
2022-01-05 21:36:28 -0300

    feat: added server configuration

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express"
+const port = process.env.PORT || 3001
+const app = express()
+
+import { setupWebSocket } from "./websocket"
+import cors from "cors"
+import http from "http"
+
+const server = new http.Server(app)
+
+setupWebSocket(server)
+
+app.use(cors())
+app.use(express.json())
+
+server.listen(port, () => console.log(`Listening on PORT ${port}`))
