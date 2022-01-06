9039228092108f4175f885987cabc1540e37259e
Leonardo Rabelo <lrabelo.mc4@digio.com.br>
2022-01-05 21:36:44 -0300

    feat: added types and templates

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export type Icon = "X" | "O" | null
+export interface Player {
+  nickname: string
+  icon: Icon
+}
+export interface Game {
+  room: string
+  turn: string
+  players: Player[]
+  winner: string | null
+  draw: boolean
+  history: Icon[]
+  status: string
+  board: (null | string)[]
+}
+export type Movement = {
+  pos: number
+  room: string
+  nickname: string
+}
